f742357628db80bb36821ef0efac72cbe8bc9f10
Tera <24725862+teraa@users.noreply.github.com>
2022-12-08 22:44:26 +0100

    update tests

--- Tests/InstanceLoaderTests.cs
+++ Tests/InstanceLoaderTests.cs
@@ -3,13 +3,20 @@
 public class InstanceLoaderTests
 {
     [Theory]
-    [InlineData("i1.txt")]
-    [InlineData("i2.txt")]
-    [InlineData("i3.txt")]
-    [InlineData("i4.txt")]
-    [InlineData("i5.txt")]
-    public void LoadInstance(string fileName)
+    [InlineData("i1.txt", 50, 200, 70, 70, 634, 200)]
+    [InlineData("i2.txt", 100, 200, 100, 100, 765, 400)]
+    [InlineData("i3.txt", 150, 700, 150, 150, 3815, 600)]
+    [InlineData("i4.txt", 200, 200, 200, 200, 1676, 800)]
+    [InlineData("i5.txt", 250, 1000, 250, 250, 7697, 1000)]
+    public void LoadInstance(string fileName, int vehicles, int capacity, int x, int y, int dueDate, int customers)
     {
-        InstanceLoader.LoadFromFile(Path.Join("../../../../instances", fileName));
+        var instance = InstanceLoader.LoadFromFile(Path.Join("../../../../instances", fileName));
+
+        instance.Vehicles.Should().Be(vehicles);
+        instance.Capacity.Should().Be(capacity);
+        instance.StartingPoint.X.Should().Be(x);
+        instance.StartingPoint.Y.Should().Be(y);
+        instance.DueDate.Should().Be(dueDate);
+        instance.Customers.Count.Should().Be(customers);
     }
 }
--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -1,2 +1,3 @@
 global using Xunit;
 global using Solvers;
+global using FluentAssertions;
