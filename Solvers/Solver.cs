f9b64953535c56400c7756de6d148ec386282f95
Tera <24725862+teraa@users.noreply.github.com>
2022-12-14 17:48:28 +0100

    add Solver.cs

---
+++ Solvers/Solver.cs
@@ -0,0 +1,14 @@
+﻿namespace Solvers;
+
+public interface ISolver
+{
+    Solution Solve(Instance instance);
+}
+
+public class Solver : ISolver
+{
+    public Solution Solve(Instance instance)
+    {
+        throw new NotImplementedException();
+    }
+}
