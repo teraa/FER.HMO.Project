b559788b0c265f44ab2a7fda6adce56905498068
Tera <24725862+teraa@users.noreply.github.com>
2022-12-18 00:41:43 +0100

    add runner

---
+++ Runner/Program.cs
@@ -0,0 +1,6 @@
+﻿using Solvers;
+
+var instance = InstanceLoader.LoadFromFile("../instances/i1.txt");
+var solver = new Solver();
+var solution = solver.Solve(instance);
+Console.WriteLine(solution);
