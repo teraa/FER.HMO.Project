451b80f935425926e0cdb129c37402e70fd0e6e5
Tera <24725862+teraa@users.noreply.github.com>
2022-12-17 23:13:33 +0100

    stops record

--- Solvers/Solution.cs
+++ Solvers/Solution.cs
@@ -8,32 +8,29 @@ public record Solution(IReadOnlyList<Route> Routes);
 
 public class Route
 {
-    private List<Customer> _customers;
+    private List<Stop> _stops;
 
     public Route(Customer depot, int capacity)
     {
         Depot = depot;
         Capacity = capacity;
-        _customers = new List<Customer>
+        _stops = new List<Stop>
         {
-            depot,
+            new(depot, 0),
         };
-
-        Time = depot.ReadyTime + depot.ServiceTime;
-        Debug.Assert(Time is 0);
     }
 
     public Customer Depot { get; }
     public int Capacity { get; }
-    public IReadOnlyList<Customer> Customers => _customers;
-    public int Time { get; private set; }
-    public Vector2 Position => _customers[^1].Position;
-    public int Demand => _customers.Sum(x => x.Demand);
-    public float Distance => _customers
+    public IReadOnlyList<Stop> Stops => _stops;
+    public Vector2 Position => _stops.Last().Customer.Position;
+    public int Demand => _stops.Sum(x => x.Customer.Demand);
+
+    public float Distance => _stops
         .Skip(1)
-        .Select(x => x.Position)
+        .Select(x => x.Customer.Position)
         .Aggregate(
-            seed: (Distance: 0f, Previous: _customers[0].Position),
+            seed: (Distance: 0f, Previous: _stops.First().Customer.Position),
             func: (acc, current) => (acc.Distance + Vector2.Distance(acc.Previous, current), current),
             resultSelector: acc => acc.Distance);
 
@@ -46,8 +43,8 @@ public class Route
             return false;
 
         var distance = Vector2.Distance(Position, customer.Position);
-        var travelTime = (int)Math.Ceiling(distance);
-        time = Time + travelTime;
+        var travelTime = (int) Math.Ceiling(distance);
+        time = _stops.Last().ServiceCompletedAt + travelTime;
 
         // Time
         if (time > customer.DueTime)
@@ -57,9 +54,6 @@ public class Route
         if (time < customer.ReadyTime)
             time = customer.ReadyTime;
 
-        // Service
-        time += customer.ServiceTime;
-
         return true;
     }
 
@@ -68,7 +62,11 @@ public class Route
         if (!CanAdd(customer, out var time))
             throw new InvalidOperationException();
 
-        _customers.Add(customer);
-        Time = time;
+        _stops.Add(new Stop(customer, time));
     }
+}
+
+public record Stop(Customer Customer, int ServiceStartedAt)
+{
+    public int ServiceCompletedAt => ServiceStartedAt + Customer.ServiceTime;
 }
