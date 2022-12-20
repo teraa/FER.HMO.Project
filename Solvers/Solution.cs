c80421425e064fa35c4eb8b8b80d774e908bda82
Tera <24725862+teraa@users.noreply.github.com>
2022-12-20 20:02:27 +0100

    add Solution#TryMove

--- Solvers/Solution.cs
+++ Solvers/Solution.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Solvers;
@@ -6,10 +7,17 @@ namespace Solvers;
 [DebuggerDisplay("{DebuggerDisplay}")]
 public class Solution
 {
-    private readonly List<Route> _routes = new();
+    private readonly List<Route> _routes;
 
     public Solution(int vehicles)
     {
+        _routes = new();
+        Vehicles = vehicles;
+    }
+
+    private Solution(List<Route> routes, int vehicles)
+    {
+        _routes = routes;
         Vehicles = vehicles;
     }
 
@@ -42,5 +50,37 @@ public class Solution
         var route = new Route(depot, capacity);
         _routes.Add(route);
         return route;
+    }
+
+    public bool TryMove(Customer customer, Route targetRoute, int targetIndex, [NotNullWhen(true)] out Solution? solution)
+    {
+        if (!_routes.Contains(targetRoute))
+            throw new ArgumentException("Target route is not a part of this solution", nameof(targetRoute));
+
+        solution = null;
+
+        if (!targetRoute.TryInsert(customer, targetIndex, out var newTargetRoute))
+            return false;
+
+        var sourceRoute = _routes.Single(
+            r => r.Stops
+                .Select(s => s.Customer)
+                .Contains(customer));
+
+        var newSourceRoute = sourceRoute.Remove(customer);
+
+        var routes = _routes.ToList();
+
+        int index = routes.IndexOf(targetRoute);
+        routes[index] = newTargetRoute;
+
+        index = routes.IndexOf(sourceRoute);
+        if (newSourceRoute.Stops.Count == 2)
+            routes.RemoveAt(index);
+        else
+            routes[index] = newSourceRoute;
+
+        solution = new Solution(routes.ToList(), Vehicles);
+        return true;
     }
 };
