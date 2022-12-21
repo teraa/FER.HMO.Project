98f1d4ecea97b6129e793b647c66d84731830669
Tera <24725862+teraa@users.noreply.github.com>
2022-12-21 01:34:34 +0100

    update runner

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,8 +1,38 @@
+using System.Diagnostics;
 using Solvers;
+using Timer = System.Timers.Timer;
+// ReSharper disable AccessToModifiedClosure
 
-var instance = InstanceLoader.LoadFromFile("../instances/i1.txt");
+const string inFile = "../instances/i1.txt";
+const string outDir = "../out/";
+Directory.CreateDirectory(outDir);
+var instance = InstanceLoader.LoadFromFile(inFile);
 var solver = new Solver();
+var incumbent = null as Solution;
+
+foreach (var time in new[] {1, 5})
+{
+    var timer = new Timer(TimeSpan.FromMinutes(time))
+    {
+        AutoReset = false,
+        Enabled = true
+    };
+    timer.Elapsed += (_, _) => WriteToFile($"{time}m", incumbent!);
+}
+
+int i = 0;
+var sw = Stopwatch.StartNew();
 await foreach (var solution in solver.SolveAsync(instance))
 {
+    incumbent = solution;
+    Console.WriteLine($"Solution {i++}: {sw.Elapsed}");
     Console.WriteLine(solution);
+}
+
+WriteToFile("un", incumbent!);
+
+void WriteToFile(string name, Solution solution)
+{
+    var outFile = Path.Combine(outDir, $"res-{name}-{Path.GetFileName(inFile)}");
+    File.WriteAllText(outFile, solution.ToString());
 }
