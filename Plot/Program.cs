76f3921067e3e83e2443e2d4e74c2e12fc6cc070
Tera <24725862+teraa@users.noreply.github.com>
2022-12-21 20:54:42 +0100

    add Plot

---
+++ Plot/Program.cs
@@ -0,0 +1,10 @@
+﻿using Solvers;
+
+var instance = InstanceLoader.LoadFromFile(args[0]);
+var points = instance.Customers
+    .Select(x => x.Position)
+    .OrderBy(p => p.X)
+    .ThenBy(p => p.Y);
+
+foreach (var point in points)
+    Console.WriteLine($"{point.X};{point.Y}");
