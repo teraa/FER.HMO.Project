35e8ae5a64d61639e9639579aff835cdb46749f1
Tera <24725862+teraa@users.noreply.github.com>
2022-12-23 00:03:11 +0100

    add TabuSolver

---
+++ Solvers/TabuSolver.cs
@@ -0,0 +1,59 @@
+using System.Runtime.CompilerServices;
+
+namespace Solvers;
+
+public class TabuSolver : ISolver
+{
+    // TODO: Fitness as:
+    // 1. Vehicle count
+    // 2. Count of lower stop count routes increased?
+    // Currently 2. is Distance
+    private readonly Queue<Customer> _tabu = new();
+
+    public int Tenure { get; set; } = 50;
+    public int? Seed { get; set; } = null;
+    public ISolver InitialSolver { get; set; } = new GreedySolver();
+
+    public async IAsyncEnumerable<Solution> SolveAsync(Instance instance,
+        [EnumeratorCancellation] CancellationToken stoppingToken = default)
+    {
+        var rnd = Seed is null
+            ? new Random()
+            : new Random(Seed.Value);
+
+        var initial = await InitialSolver.SolveFinalAsync(instance, stoppingToken);
+        var incumbent = initial;
+        var previous = initial;
+
+        yield return incumbent;
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var customer = instance.Customers
+                .Except(_tabu)
+                .Skip(rnd.Next(0, instance.Customers.Count - _tabu.Count))
+                .First();
+
+            var route = previous.Routes[rnd.Next(0, previous.Routes.Count)];
+            var index = rnd.Next(1, route.Stops.Count - 1);
+
+            if (!previous.TryMove(customer, route, index, out var current))
+                continue;
+
+            if (current.Distance < incumbent.Distance || current.Routes.Count < incumbent.Routes.Count)
+            {
+                yield return incumbent = current;
+            }
+
+            if (current.Distance < previous.Distance || current.Routes.Count < previous.Routes.Count)
+            {
+                _tabu.Enqueue(customer);
+
+                if (_tabu.Count > Tenure)
+                    _tabu.Dequeue();
+            }
+
+            previous = current;
+        }
+    }
+}
