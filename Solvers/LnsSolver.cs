c89d71385e08b05335f2cd32e1f5b70223b35357
Tera <24725862+teraa@users.noreply.github.com>
2022-12-23 19:15:32 +0100

    copy LnsSolver

---
+++ Solvers/LnsSolver.cs
@@ -0,0 +1,41 @@
+using System.Runtime.CompilerServices;
+using Solvers.Types;
+
+namespace Solvers;
+
+public class LnsSolver : ISolver
+{
+    public int? Seed { get; set; }
+    public ISolver InitialSolver { get; set; } = new GreedySolver();
+
+    public async IAsyncEnumerable<Solution> SolveAsync(Instance instance,
+        [EnumeratorCancellation] CancellationToken stoppingToken = default)
+    {
+        var rnd = Seed is null
+            ? new Random()
+            : new Random(Seed.Value);
+
+        var initial = await InitialSolver.SolveFinalAsync(instance, stoppingToken);
+        var incumbent = initial;
+        var previous = initial;
+
+        yield return incumbent;
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var customer = instance.Customers[rnd.Next(0, instance.Customers.Count)];
+            var route = previous.Routes[rnd.Next(0, previous.Routes.Count)];
+            var index = rnd.Next(1, route.Stops.Count - 1);
+
+            if (!previous.TryMove(customer, route, index, out var current))
+                continue;
+
+            if (current.Distance < incumbent.Distance || current.Routes.Count < incumbent.Routes.Count)
+            {
+                yield return incumbent = current;
+            }
+
+            previous = current;
+        }
+    }
+}
