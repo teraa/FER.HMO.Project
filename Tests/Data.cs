42483016bf8b6a69fcbda5125135818091567f2b
Tera <24725862+teraa@users.noreply.github.com>
2022-12-26 17:18:32 +0100

    make test data properties instead of fields

--- Tests/Data.cs
+++ Tests/Data.cs
@@ -5,6 +5,6 @@ namespace Tests;
 
 public static class Data
 {
-    public static readonly Customer Depot = new(0, Vector2.Zero, 0, 0, 0, 0);
-    public static readonly Customer Customer = Depot with {Id = 1};
+    public static Customer Depot => new(0, Vector2.Zero, 0, 0, 0, 0);
+    public static Customer Customer => Depot with {Id = 1};
 }
